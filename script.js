e07f1f3365f0ada5eacc0098e58eefc5dc38001d
Gustavo_C <gustavoscamargo@outlook.com>
2024-01-14 15:47:24 +0000

    Calculator script

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
-const buttons = document.querySelectorAll('button');
-const screen = document.querySelector('.screen');
+let currentNumber = '0';
+let displayNumber = false;
+
+const updateNumber = (value) => {
+  if (currentNumber === '0' || displayNumber) {
+    currentNumber = value;
+  } else {
+    currentNumber += value;
+  }
+
+  displayNumber = false;
+
+  updateScreen();
+};
+
+const updateScreen = () => {
+  const screen = document.querySelector('.screen');
+  screen.textContent = currentNumber;
+};
+
+const result = () => {
+  const result = eval(currentNumber);
+  currentNumber += ` = ${result}`;
+
+  updateScreen();
+
+  displayNumber = true;
+};
+
+const removeLastNumber = () => {
+  currentNumber = currentNumber.slice(0, -1);
+
+  if (currentNumber === "") {
+    currentNumber = "0";
+  }
+
+  updateScreen();
+};
+
+const clearScreen = () => {
+  currentNumber = '0';
+  displayNumber = false;
+
+  updateScreen();
+}
